f590ea86f0e2dd91f2468f4a8a35ded339f1fdbc
Aman Punekar <68313251+AmanPunekar@users.noreply.github.com>
2024-03-14 18:07:29 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
-const express = require("express");
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDb!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
