72b5af5111e914b9d532108b27c76e487f6e129c
Aman Punekar <68313251+AmanPunekar@users.noreply.github.com>
2024-03-17 20:46:06 +0530

    complete all functionalities of website

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -14,6 +15,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json()); //allow json as a input of server
 app.use(cookieParser());
@@ -24,6 +28,11 @@ app.listen(8080, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
